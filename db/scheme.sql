d9974425265f148761034dc8b19953e217cf32b8
Liciaav <liciaavasc@gmail.com>
2024-10-23 21:15:50 -0400

    update folder db, folder src and packge.json.

--- db/scheme.sql
+++ db/scheme.sql
@@ -12,7 +12,8 @@ CREATE TABLE role (
     title: VARCHAR(30) UNIQUE NOT NULL,
     salary: DECIMAL NOT NULL,
     department_id: INTEGER NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES departament(id)
+    FOREIGN KEY (department_id) 
+    REFERENCES departament(id)
     ON DELETE SET NULL
 );
 
@@ -22,7 +23,9 @@ CREATE TABLE employee (
     last_name: VARCHAR(30) NOT NULL,
     role_id: INTEGER NOT NULL,
     manager_id: INTEGER,
-    FOREIGN KEY (role_id) REFERENCES role(id)
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (role_id) 
+    REFERENCES role(id)
+    FOREIGN KEY (manager_id) 
+    REFERENCES employee(id)
     ON DELETE SET NULL
 );
